3d59e7c38818a3697a2f385b4143463dbcde31dc
bhjadav24888 <bhjadav24888@gmail.com>
2024-10-22 17:18:03 +0530

    code updated
    code updated

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require("cypress");
 	module.exports = defineConfig({
 	  e2e: {
 		specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensure it matches your file pattern
-		baseUrl: 'http://127.0.0.1:9292',
+		baseUrl: 'https://shopybhk.myshopify.com',
 	  },
 });
