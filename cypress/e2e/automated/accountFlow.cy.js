a8d0eb7df3624410493df62d7d361818699c7eae
bhjadav24888 <bhjadav24888@gmail.com>
2024-11-14 21:51:32 +0530

    add file accountflow
    automated testing add file  accountFlow

---
+++ cypress/e2e/automated/accountFlow.cy.js
@@ -0,0 +1,34 @@
+describe('Shopify Full User Flow', () => {
+  it('searches for a product, adds it to the cart, and proceeds to checkout', () => {
+    
+	// Visit the homepage
+    cy.visit('https://shopybhk.myshopify.com/');
+	
+	//enter store password
+	//cy.get('.password-link').click();
+	cy.get('[type="password"]').type('bratwu');
+	cy.get('[type="submit"]').click();
+	//cy.get('#accept-cookies').click();
+	
+	//home page
+	//cy.get('.caption-secondary').first().click();
+	//plp page
+	//cy.get('.product.item').first().click();
+	
+	//select product varient
+	//cy.get('.header-logo').trigger('mouseover');
+	
+	//pdp page add to cart
+	//cy.get('.button-addtobag-cta-title').first().click({ force: true });
+	
+	//bag icone mousehover
+	//cy.get('.header-item-bag').trigger('mouseover');
+	
+	//checkout click
+	//cy.get('.button.button-orange').click({ force: true });
+	
+	// Confirm order was placed
+    //cy.contains('automated testing done');
+	
+  });
+});
