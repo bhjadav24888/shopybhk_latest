5e550231555786f9886754d2fe59d64406ca5a9f
bhjadav24888 <bhjadav24888@gmail.com>
2024-11-15 18:21:58 +0530

    automation testing config file code change
    automation testing config file code change

--- automated_testing/cypress.config.js
+++ automated_testing/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 	module.exports = defineConfig({
 	  e2e: {
-		specPattern: 'cypress/e2e/**/*.cy.{js,ts}', // Ensure it matches your file pattern
+		specPattern: 'automated_testing/cypress/e2e/**/*.cy.{js,ts}', // Ensure it matches your file pattern
 		baseUrl: 'https://shopybhk.myshopify.com',
 	  },
 });
