f1a5affee93224ee08f03ca75cb79af7aca8407d
bhjadav24888 <bhjadav24888@gmail.com>
2024-11-15 18:52:01 +0530

    config file code change
    config file code change

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,6 @@ const { defineConfig } = require("cypress");
 	  e2e: {
 		specPattern: 'automated_testing/cypress/e2e/**/*.cy.{js,ts}', // Ensure it matches your file pattern
 		baseUrl: 'https://shopybhk.myshopify.com',
+		supportFile: false, // Disable support file
 	  },
 });
